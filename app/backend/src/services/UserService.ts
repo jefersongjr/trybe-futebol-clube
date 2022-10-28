7829569d662084ff682d43b55fa9d9127d51f9d1
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-28 16:00:29 -0300

    fix: validando senha criptografada

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,17 +1,19 @@
-import { ILogin } from '../interfaces';
 import User from '../database/models/UserModel'
 import { tokenGenerate } from '../utils/tokenGenerate';
+import ThrowException from '../middlewares/exceptions/ThrowException';
 import * as bcrypt from 'bcryptjs'
 
 export class UserService {
     public getLogin = async ( email: string , password: string ) => {
-        const user = await User.findOne({ where: { email }})
-        if (!user) throw new ThrowException(401, 'Username or password invalid');
-        
+        if (!email || !password) throw new ThrowException(400, 'All fields must be filled');
 
+        const user = await User.findOne({ where: { email }})
+
+        if (!user) throw new ThrowException(401, 'Incorrect email or password');
+        
         const validatePass = bcrypt.compareSync(password, user?.password);
 
-        if (!user) throw new ThrowException(401, 'Username or password invalid');
+        if (!validatePass) throw new ThrowException(401, 'Incorrect email or password');
 
 
         const token = tokenGenerate.makeToken(user);
