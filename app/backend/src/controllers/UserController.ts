fc11fd91849066495bcf0069aba5b5fb37d6d19b
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-28 17:07:47 -0300

    fix: instanceando service no constructor

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,14 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { UserService } from '../services/UserService';
 
-const userService = new UserService()
 
 export class UserController {
+    private userService : UserService;
+
+    constructor() {
+        this.userService = new UserService();
+    }
+
     public getLogin = async (req: Request, res: Response, next: NextFunction) => {
         try {
         const { email, password } = req.body;
       
-        const token = await userService.getLogin(email, password);
+        const token = await this.userService.getLogin(email, password);
         return res.status(200).json({ token: token });  
         } catch (error) {
            next(error) ;
