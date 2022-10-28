5922512762ecfb5b5621e2326677bb241691a388
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-28 16:00:48 -0300

    fix: adicionando bloco de exceção

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,13 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { UserService } from '../services/UserService';
 
 const userService = new UserService()
 
 export class UserController {
-    public getLogin = async (req: Request, res: Response) => {
+    public getLogin = async (req: Request, res: Response, next: NextFunction) => {
+        try {
         const { email, password } = req.body;
       
-    const token = await userService.getLogin(email, password);
-    return res.status(200).json({ token: token });
+        const token = await userService.getLogin(email, password);
+        return res.status(200).json({ token: token });  
+        } catch (error) {
+           next(error) ;
+        }
     }
 }
