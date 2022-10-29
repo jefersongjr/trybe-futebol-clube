18561518206a492c68a69b91d36ef59b1868ea66
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-29 01:13:33 -0300

    feat: criando testes para rota login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -88,4 +88,54 @@ describe('testando o EndPoint "/login"', () => {
             expect(httpResponse.body).to.deep.equal({message: 'All fields must be filled'})
           });
         });
+
+        describe('quando o email estiver incorreto ou não cadastrado', () => {
+          const user = {
+            username: 'Admin',
+            role: 'admin',
+            email: 'admin@admin.com',
+            password: 'secret_admin'
+          };
+      
+          beforeEach(() => {
+            sinon.stub(User, 'findOne').resolves(null);
+           });
+      
+           afterEach(() => sinon.restore())
+      
+          
+          it('deve retornar o status 401', async () => {
+              const httpResponse = await chai
+                  .request(app)
+                  .post('/login')
+                  .send({email: 'emailerrado@admin.com', password: 'admin_secret'});
+              expect(httpResponse.status).to.be.equal(401);
+              expect(httpResponse.body).to.deep.equal({message: 'Incorrect email or password'})
+            });
+          });
+
+          describe('quando o password estiver incorreto ou não cadastrado', () => {
+            const user = {
+              username: 'Admin',
+              role: 'admin',
+              email: 'admin@admin.com',
+              password: 'kdiadgahdajdkmsadasbdyasbdiapdmalsdsbdad'
+            };
+        
+            beforeEach(() => {
+              sinon.stub(bcrypt, 'compareSync').resolves(false);
+            });
+        
+             afterEach(() => sinon.restore())
+        
+            
+            it('deve retornar o status 401', async () => {
+                const httpResponse = await chai
+                    .request(app)
+                    .post('/login')
+                    .send({email: 'emailerrado@admin.com', password: 'senha_errada'});
+                expect(httpResponse.status).to.be.equal(401);
+                expect(httpResponse.body).to.deep.equal({message: 'Incorrect email or password'})
+              });
+            });
     });
