8271a5cf1e4cdc2e5ea1ac7ed76a608a5bbf1750
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-29 01:05:19 -0300

    feat: criando teste para login valido

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -2,11 +2,13 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import * as bcrypt from 'bcryptjs';
 
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import User from '../database/models/UserModel';
 
 chai.use(chaiHttp);
 
@@ -38,13 +40,38 @@ describe('testando o EndPoint "/login"', () => {
 
   //   expect(...)
   // });
+
+  describe('quando a requisição for feita corretamente', () => {
+    const user = {
+      username: 'Admin',
+      role: 'admin',
+      email: 'admin@admin.com',
+      password: 'secret_admin'
+    };
+
+    beforeEach(() => {
+      sinon.stub(User, 'findOne').resolves(user as User);
+      sinon.stub(bcrypt, 'compareSync').resolves(true);
+     });
+
+     afterEach(() => sinon.restore())
+
+    
+    it('deve retornar o status 200', async () => {
+        const httpResponse = await chai
+            .request(app)
+            .post('/login')
+            .send({email: 'admin@admin.com', password: 'admin_secret'});
+        expect(httpResponse.status).to.be.equal(200);
+      });
+    });
+
   describe('quando o campo email não for informado', () => {
     it('deve retornar o status 400', async () => {
         const httpResponse = await chai
             .request(app)
             .post('/login')
-            .send({password: 'admin_secret'})
-            ;
+            .send({password: 'admin_secret'});
         expect(httpResponse.status).to.equal(400);
         expect(httpResponse.body).to.deep.equal({message: 'All fields must be filled'})
       });
@@ -61,4 +88,4 @@ describe('testando o EndPoint "/login"', () => {
             expect(httpResponse.body).to.deep.equal({message: 'All fields must be filled'})
           });
         });
-});
+    });
